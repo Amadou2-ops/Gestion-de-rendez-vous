4461d5b5a31b51c3ac016f035262736b57129681
Amadou2-ops <78507045+Amadou2-ops@users.noreply.github.com>
2021-11-27 20:43:54 +0100

    Add files via upload

---
+++ test/NewEmptyTestNGTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+
+import static org.testng.Assert.*;
+
+/**
+ *
+ * @author HP
+ */
+public class NewEmptyTestNGTest {
+
+    public NewEmptyTestNGTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+}
